dc9a64299ced9547245f647cf7b8ad237da695a6
Dan Mills <thunder@mozilla.com>
2007-09-12 16:34:41 -0700

    add an optional 'xpi' argument to build.sh; only create the xpi if given, and if not then craft a manifest for direct (unpacked) use

--- build.sh
+++ build.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
+#!/bin/sh -x
+
+substitutions="unpacked jar"
+unpacked=
+jar="# "
+
+if [[ $1 == xpi ]]; then
+    unpacked="# "
+    jar=
+fi
+
+for in in `find . -type f -name \*.in`; do
+    out=`echo $in | sed 's/\.in$//'`
+    cp $in $out
+    for subst in $substitutions; do
+        sed -e "s/@$subst@/${!subst}/g" $out > $out.tmp
+        mv $out.tmp $out
+    done
+done
+
+if [[ ! $1 == xpi ]]; then
+    exit 0;
+fi
 
 cd chrome
 zip -9 -ur sync.jar *
