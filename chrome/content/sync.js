6e4ba55380f95d0dfbc82c94de95c4f78fa028cf
Dan Mills <thunder@mozilla.com>
2007-09-26 15:28:01 -0700

    unify logging routines, log via the component.  Log to a file.  Add a couple of crappy attempts at nicer logging output

--- chrome/content/sync.js
+++ chrome/content/sync.js
@@ -1,3 +1,49 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Bookmarks Sync.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Dan Mills <thunder@mozilla.com>
+ *  Chris Beard <cbeard@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const MODE_RDONLY   = 0x01;
+const MODE_WRONLY   = 0x02;
+const MODE_CREATE   = 0x08;
+const MODE_APPEND   = 0x10;
+const MODE_TRUNCATE = 0x20;
+
+const PERMS_FILE      = 0644;
+const PERMS_DIRECTORY = 0755;
+
 function Sync() {
   this._init();
 }
@@ -26,7 +72,7 @@ Sync.prototype = {
   },
 
   _onLogin: function Sync__onLogin() {
-    LOG("Login successful");
+    this._ss.notice("Login successful");
 
     let status1 = document.getElementById("sync-menu-status");
     if(status1) {
@@ -140,7 +186,7 @@ Sync.prototype = {
   },
 
   doCancelSync: function Sync_doCancelSync(event) {
-    LOG("cancel sync unimplemented");
+    this._ss.notice("cancel sync unimplemented");
   },
 
   doOpenPrefs: function Sync_doOpenPrefs(event) {
@@ -235,13 +281,6 @@ Sync.prototype = {
     }
   }
 };
-
-function LOG(aText) {
-  dump(aText + "\n");
-  var consoleService = Cc["@mozilla.org/consoleservice;1"].
-                       getService(Ci.nsIConsoleService);
-  consoleService.logStringMessage(aText);
-}
 
 let gSync = new Sync();
 
