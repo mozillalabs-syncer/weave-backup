18d330b060a56c8053bbfacd70d70e78571b0c1a
Myk Melez <myk@mozilla.com>
2007-12-21 00:11:50 -0800

    make log dialog localizable

--- chrome/content/log.js
+++ chrome/content/log.js
@@ -2,16 +2,14 @@ var Ci = Components.interfaces;
 var Cc = Components.classes;
 var Cr = Components.results;
 
-const MODE_RDONLY   = 0x01;
-const MODE_WRONLY   = 0x02;
-const MODE_CREATE   = 0x08;
-const MODE_APPEND   = 0x10;
-const MODE_TRUNCATE = 0x20;
+let gSyncLog = {
+  get _stringBundle() {
+    let stringBundle = document.getElementById("weaveStringBundle");
+    delete this._stringBundle;
+    this._stringBundle = stringBundle;
+    return this._stringBundle;
+  },
 
-const PERMS_FILE      = 0644;
-const PERMS_DIRECTORY = 0755;
-
-gSyncLog = {
   init: function() {
     let tabbox = document.getElementById("syncLogTabs");
     let index = document.getElementById("extensions.weave.log.selectedTabIndex");
@@ -53,43 +51,44 @@ gSyncLog = {
     else
       document.getElementById("sync-log-verbose-frame").
         setAttribute("src", "chrome://weave/content/default-log.txt");
-  }
-}
+  },
 
-function saveAs() {
-  let tabbox = document.getElementById("syncLogTabs");
-  let index = tabbox.selectedIndex;
-
-  let dirSvc = Cc["@mozilla.org/file/directory_service;1"].
-  getService(Ci.nsIProperties);
-
-  let file = dirSvc.get("ProfD", Ci.nsIFile);
-  file.QueryInterface(Ci.nsILocalFile);
-
-  file.append("weave");
-  file.append("logs");
-
-  if (index == 0)
-    file.append("brief-log.txt");
-  else
-    file.append("verbose-log.txt");
-
-  if (!file.exists()) {
-    alert("No log available");
-    return;
-  }
-
-  let backupsDir = dirSvc.get("Desk", Ci.nsILocalFile);
-  let fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
-  fp.init(window, "Choose Destination File", Ci.nsIFilePicker.modeSave);
-  fp.appendFilters(Ci.nsIFilePicker.filterAll);
-  fp.displayDirectory = backupsDir;
-  fp.defaultString = "Weave Sync.log";
-
-  if (fp.show() != Ci.nsIFilePicker.returnCancel) {
-    if (fp.file.exists())
-      fp.file.remove(false);
-    file.copyTo(fp.file.parent, fp.file.leafName);
+  saveAs: function() {
+    let tabbox = document.getElementById("syncLogTabs");
+    let index = tabbox.selectedIndex;
+  
+    let dirSvc = Cc["@mozilla.org/file/directory_service;1"].
+    getService(Ci.nsIProperties);
+  
+    let file = dirSvc.get("ProfD", Ci.nsIFile);
+    file.QueryInterface(Ci.nsILocalFile);
+  
+    file.append("weave");
+    file.append("logs");
+  
+    if (index == 0)
+      file.append("brief-log.txt");
+    else
+      file.append("verbose-log.txt");
+  
+    if (!file.exists()) {
+      alert(this._stringBundle.getString("noLogAvailable.alert"));
+      return;
+    }
+  
+    let backupsDir = dirSvc.get("Desk", Ci.nsILocalFile);
+    let fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
+    let filePickerTitle = this._stringBundle.getString("filePicker.title");
+    fp.init(window, filePickerTitle, Ci.nsIFilePicker.modeSave);
+    fp.appendFilters(Ci.nsIFilePicker.filterAll);
+    fp.displayDirectory = backupsDir;
+    fp.defaultString = "Weave Sync.log";
+  
+    if (fp.show() != Ci.nsIFilePicker.returnCancel) {
+      if (fp.file.exists())
+        fp.file.remove(false);
+      file.copyTo(fp.file.parent, fp.file.leafName);
+    }
   }
 }
 
