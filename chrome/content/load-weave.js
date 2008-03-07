b0d172a52c10ea8743056f0190c47366595a587b
Dan Mills <thunder@mozilla.com>
2008-03-07 01:56:36 -0800

    Asynchronous generator helpers rework + PKI work * Async helpers are in a module of their own now * Async routines have simpler semantics now.  onComplete handlers are taken care of by the helpers.  Exceptions are bubbled up across nested asynchronous generators * Stack traces are automatically logged for unhandled exceptions * Async generators are now allowed to 'bottom out' (StopIteration is ignored) - this is configurable. * RSA key generation fixes * On login we now create an RSA keypair, encrypt the private one with PBE, and upload them to the server * Log files are now limited to 2MB (down from 5)

--- chrome/content/load-weave.js
+++ chrome/content/load-weave.js
@@ -19,6 +19,7 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://weave/constants.js");
 Components.utils.import("resource://weave/log4moz.js");
 Components.utils.import("resource://weave/util.js");
+Components.utils.import("resource://weave/async.js");
 Components.utils.import("resource://weave/crypto.js");
 Components.utils.import("resource://weave/identity.js");
 Components.utils.import("resource://weave/dav.js");
