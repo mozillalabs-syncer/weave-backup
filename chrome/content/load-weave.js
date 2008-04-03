170f1d226089b4581d1e6e350716d8905de5d305
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2008-04-03 14:26:06 -0700

    Fixed bugs in cookieSyncCore.itemExists and cookieStore.addCommand.  The problem in addCommand was just a missing argument, but in itemExists I had to change the implementation to use cookieManager.enumerator rather than cookieManager.findMatchingCookie -- the latter function apparently does not exist in the nsICookieManager2 interface despite what MDC says about it.

--- chrome/content/load-weave.js
+++ chrome/content/load-weave.js
@@ -19,7 +19,7 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://weave/constants.js");
 Components.utils.import("resource://weave/log4moz.js");
 Components.utils.import("resource://weave/util.js");
-Components.utils.import("resource://weave/async.js");
+Components.utils.import("resource://weave/async.js"); //File not found??  That's weird, it's totally there.
 Components.utils.import("resource://weave/wrap.js");
 Components.utils.import("resource://weave/crypto.js");
 Components.utils.import("resource://weave/identity.js");
