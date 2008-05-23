7deb7a989e20265cc67bb9a2d674f45d8a1f851a
Atul Varma <varmaa@toolness.com>
2008-05-22 17:24:10 -0700

    Added more documentation.

--- ensure_xpis_are_consistent.py
+++ ensure_xpis_are_consistent.py
@@ -1,10 +1,18 @@
+"""
+    This script tries to ensure that the XPIs passed to it are
+    consistent with each other.  It does this by treating the first
+    XPI as a "canonical" XPI, and ensuring that all common files
+    between the canonical XPI and the rest of the XPIs are
+    identical to one another.
+"""
+
 import sys
 import zipfile
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 2:
-        print "usage: %s <file1.xpi> <file2.xpi> ... [fileN.xpi]"
+        print "usage: %s <canonical-xpi> <file-1> ... [file-N]"
         sys.exit(1)
 
     canonical = zipfile.ZipFile(args[0], "r")
