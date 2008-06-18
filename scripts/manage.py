3bbceb534632c01033b45f84443a6298281f4111
Atul Varma <varmaa@toolness.com>
2008-06-18 11:11:58 -0700

    Moved the test functionality from manage.py into a new script, runtests.py, and decided to use nose, which will allow us to easy add multiple types of tests and have them all be run automatically.  This move alone has added 4 tests to the test suite from the scripts/ directory, which weren't previously run by 'manage.py test' or the Makefile system.

--- scripts/manage.py
+++ scripts/manage.py
@@ -1,77 +1,6 @@
 import os
 import sys
 import xml.dom.minidom
-import subprocess
-import difflib
-
-TEST_DIRS = [os.path.join("tests", "unit"),
-             os.path.join("tests", "system")]
-
-def find_tests():
-    tests = []
-    for dirname in TEST_DIRS:
-        testfiles = [os.path.join(dirname, filename)
-                     for filename in os.listdir(dirname)
-                     if (filename.startswith("test_") and
-                         filename.endswith(".js"))]
-        tests.extend(testfiles)
-    return tests
-
-def run_test(test):
-    dirname = os.path.dirname(test)
-    testname = os.path.splitext(os.path.basename(test))[0]
-    print "%-25s: " % testname,
-    result = subprocess.call(
-        ["make",
-         "-C", dirname,
-         testname],
-        stdout = subprocess.PIPE,
-        stderr = subprocess.STDOUT
-        )
-    logfile_name = os.path.join(dirname, testname + ".log")
-    if result != 0:
-        print "FAIL"
-        return open(logfile_name, "r").read()
-    else:
-        expected_logfile_name = logfile_name + ".expected"
-        if os.path.exists(expected_logfile_name):
-            expected = open(expected_logfile_name, "r").read()
-            actual = open(logfile_name, "r").read()
-            if expected != actual:
-                print "FAIL"
-                diff = "Expected results differ from actual results.\n\n"
-                diff += "\n".join(difflib.unified_diff(
-                    expected.splitlines(), actual.splitlines(),
-                    "expected results", "actual results"
-                    ))
-                return diff
-        print "PASS"
-        return None
-
-def run_tests(testnames = None):
-    tests = find_tests()
-    if testnames:
-        newtests = []
-        for testname in testnames:
-            for test in tests:
-                if testname in test:
-                    newtests.append(test)
-        tests = newtests
-    errors = {}
-    if not tests:
-        print "No tests found!"
-        sys.exit(1)
-    for test in tests:
-        errors[test] = run_test(test)
-    failed_tests = [test for test in tests
-                    if errors[test] != None]
-    for test in failed_tests:
-        print "-" * 40
-        print "Output of %s:" % test
-        print
-        print errors[test]
-    if failed_tests:
-        sys.exit(1)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -80,7 +9,6 @@ if __name__ == "__main__":
         print
         print "'command' can be one of the following:"
         print
-        print "    test - run unit tests, optionally specifying tests to run"
         print "    install - install to the given profile dir"
         print "    uninstall - uninstall from the given profile dir"
         print
@@ -92,10 +20,8 @@ if __name__ == "__main__":
     path_to_extension_root = mydir
 
     cmd = args[0]
-    
-    if cmd == "test":
-        run_tests(args[1:])
-    elif cmd in ["install", "uninstall"]:
+
+    if cmd in ["install", "uninstall"]:
         if len(args) != 2:
             print "Path to profile directory not supplied."
             sys.exit(1)
