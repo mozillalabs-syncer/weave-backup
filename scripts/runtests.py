e8c2d73cc9479f679c81015f264c0b3449d7e0d1
Atul Varma <varmaa@toolness.com>
2008-06-23 17:36:25 -0700

    Added a --revalidate-log option to runtests.py to make logtest re-validation easier.

--- scripts/runtests.py
+++ scripts/runtests.py
@@ -3,12 +3,27 @@ import sys
 import difflib
 import unittest
 import subprocess
+import distutils.file_util
 
 import getnose
 import nose.plugins
 import nose.plugins.builtin
 
 class JsTest(nose.plugins.Plugin):
+    def options(self, parser, env=os.environ):
+        nose.plugins.Plugin.options(self, parser, env)
+        parser.add_option("--revalidate-log", action="store_true",
+                          dest="revalidate_log",
+                          default=False,
+                          help="For all tests run, makes their logs "
+                          "the expected, or canonical, log file to "
+                          "which all future runs of the tests are "
+                          "compared to.")
+
+    def configure(self, options, config):
+        nose.plugins.Plugin.configure(self, options, config)
+        self.revalidate_log = options.revalidate_log
+
     def wantFile(self, file):
         basename = os.path.basename(file)
         ext = os.path.splitext(file)[1]
@@ -19,11 +34,12 @@ class JsTest(nose.plugins.Plugin):
         return None
 
     def loadTestsFromFile(self, filename):
-        return [JsTestCase(filename)]
+        return [JsTestCase(filename, self.revalidate_log)]
 
 class JsTestCase(unittest.TestCase):
-    def __init__(self, test):
+    def __init__(self, test, revalidate_log):
         self.__test = test
+        self.__revalidate_log = revalidate_log
         unittest.TestCase.__init__(self)
 
     def shortDescription(self):
@@ -45,6 +61,9 @@ class JsTestCase(unittest.TestCase):
             self.fail(open(logfile_name, "r").read())
         else:
             expected_logfile_name = logfile_name + ".expected"
+            if self.__revalidate_log:
+                distutils.file_util.copy_file(logfile_name,
+                                              expected_logfile_name)
             if os.path.exists(expected_logfile_name):
                 expected = open(expected_logfile_name, "r").read()
                 actual = open(logfile_name, "r").read()
