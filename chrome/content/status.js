fa02686259732fe2b4dc442f9e9cbb160e28a42f
Dan Mills <thunder@mozilla.com>
2008-06-30 14:00:55 -0700

    change onQuit dialog to wait for a running sync in order to start the last sync

--- chrome/content/status.js
+++ chrome/content/status.js
@@ -41,22 +41,39 @@ let WeaveStatus = {
     this._os.addObserver(this, "weave:service:sync:start", true);
     this._os.addObserver(this, "weave:service:sync:success", true);
     this._os.addObserver(this, "weave:service:sync:error", true);
+    this._os.addObserver(this, "weave:service:global:success", true);
+    this._os.addObserver(this, "weave:service:global:error", true);
 
-    try {
-      Weave.Service.sync();
+    // FIXME: we should set a timer to force quit in case there is a
+    // stale local lock
+    if (Weave.DAV.locked) {
+      this._log.info("waiting for current action to finish");
+      this._existingSync = true;
+      this._statusBox.setAttribute("status", "active");
+      this._statusText.value = this._stringBundle.getString("status.active");
+    } else {
+      this.doSync();
     }
-    catch(ex) {
-      this._log.error("error starting quit sync: " + ex);
-      window.close();
-    }
-
-    // XXX Should we set a timeout to cancel sync if it takes too long?
   },
 
   onUnload: function WeaveStatus_onUnload() {
     this._os.removeObserver(this, "weave:service:sync:start");
     this._os.removeObserver(this, "weave:service:sync:success");
     this._os.removeObserver(this, "weave:service:sync:error");
+    this._os.removeObserver(this, "weave:service:global:success");
+    this._os.removeObserver(this, "weave:service:global:error");
+  },
+
+  doSync: function WeaveStatus_doSync() {
+    try {
+      // XXX Should we set a timeout to cancel sync if it takes too long?
+      if (Weave.Service.isLoggedIn)
+        Weave.Service.sync();
+    }
+    catch(ex) {
+      this._log.error("error starting quit sync: " + ex);
+      window.close();
+    }
   },
 
   // nsIObserver
@@ -72,6 +89,9 @@ let WeaveStatus = {
         break;
 
       case "weave:service:sync:success":
+        if (this._existingSync)
+          break;
+
         this._log.info("quit sync succeeded");
 
         this._statusBox.setAttribute("status", "success");
@@ -86,6 +106,9 @@ let WeaveStatus = {
         break;
 
       case "weave:service:sync:error":
+        if (this._existingSync)
+          break;
+
         this._log.info("quit sync failed");
 
         this._statusBox.setAttribute("status", "error");
@@ -98,6 +121,15 @@ let WeaveStatus = {
         // FIXME: send a growl or other low-priority notification, or don't exit
         // and let the user try again.
 
+        break;
+
+      case "weave:service:global:success":
+      case "weave:service:global:error":
+        if (this._existingSync && !Weave.DAV.locked) {
+          this._log.info("existing action finished, starting quit sync");
+          this._existingSync = false;
+          this.doSync();
+        }
         break;
     }
   }
