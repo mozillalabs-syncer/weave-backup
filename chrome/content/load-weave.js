9774dd8afbf572fb7e6cec2dfd104f88ce1925b8
Dan Mills <thunder@mozilla.com>
2008-08-20 18:24:08 -0700

    Mozilla has built-in support for creating resource:// URIs now via the manifest.  Use that instead of a manual hack.

--- chrome/content/load-weave.js
+++ chrome/content/load-weave.js
@@ -1,17 +1,3 @@
-let ioSvc = Components.classes["@mozilla.org/network/io-service;1"]
-  .getService(Components.interfaces.nsIIOService);
-let resProt = ioSvc.getProtocolHandler("resource")
-  .QueryInterface(Components.interfaces.nsIResProtocolHandler);
-
-if (!resProt.hasSubstitution("weave")) {
-  let extMgr = Components.classes["@mozilla.org/extensions/manager;1"]
-    .getService(Components.interfaces.nsIExtensionManager);
-  let loc = extMgr.getInstallLocation("{340c2bbc-ce74-4362-90b5-7c26312808ef}");
-  let extD = loc.getItemLocation("{340c2bbc-ce74-4362-90b5-7c26312808ef}");
-  extD.append("modules");
-  resProt.setSubstitution("weave", ioSvc.newFileURI(extD));
-}
-
 // These are here because of bug 408412.
 // Note: they are here depth-first so that it is *this* import() which
 // triggers the first exception, otherwise it'll be obscured (see the bug).
