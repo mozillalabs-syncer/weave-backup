1da09613f0364ef8288ac2f7694efa4e2e851ef0
Daniel Brooks <db48x@db48x.net>
2008-11-21 06:45:57 -0600

    tweak gen-buildid.sh so that it works no matter what the default output style is

--- build/gen-buildid.sh
+++ build/gen-buildid.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-hg tip 2>/dev/null | grep ^changeset | awk '{print $2}' | awk -F: '{print $1}'
+hg tip --style=default 2>/dev/null | grep ^changeset | awk '{print $2}' | awk -F: '{print $1}'
+
