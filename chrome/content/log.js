804d46b56c3d7e372d62c7b07cd3ad68587d246f
Edward Lee <edward.lee@engineering.uiuc.edu>
2009-02-28 11:11:36 -0800

    Bug 480528 - Color Weave logs to help find errors, engine problems, etc.

--- chrome/content/log.js
+++ chrome/content/log.js
@@ -54,8 +54,68 @@ let gSyncLog = {
 
     for each (let type in this._logTypes) {
       let frame = this._frame(type);
+
+      // The first frame load is of the text log
+      let handleLoad = function SyncLog__handleLoad(event) {
+        frame.removeEventListener("load", handleLoad, true);
+        gSyncLog.onFrameLoad(event, frame);
+      };
+      frame.addEventListener("load", handleLoad, true);
       frame.setAttribute("src", this._uriLog(type));
     }
+  },
+
+  onFrameLoad: function SyncLog_onFrameLoad(event, frame) {
+    let text = frame.contentDocument.documentElement.lastChild.textContent;
+    let re = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\t([^\\s]+)\\s+([^\\s]+)\t.*";
+    let matches = text.match(new RegExp(re, "g"));
+
+    // No need to process the text if there's nothing to color
+    if (!matches)
+      return;
+
+    // Define some colors for various levels/types of logging
+    let textColors = {
+      WARN:   "#f90",
+      ERROR:  "#f30",
+      FATAL:  "#f00",
+
+      CONFIG: "#600",
+      DEBUG:  "#060",
+      INFO:   "#006",
+      TRACE:  "#066",
+    };
+
+    let logPattern = new RegExp(re);
+    // Color one line at a time on a timeout reverse-log-order
+    let colorText = function SyncLog__colorText(doc) setTimeout(function() {
+      let [line, source, type] = matches.pop().match(logPattern);
+
+      // Generate a background color based on the source of the log
+      let bgColor = [0, 0, 0];
+      for (let i = source.length; --i >= 0; )
+        bgColor[i % 3] += source.charCodeAt(i);
+      bgColor = bgColor.map(function(v) v % 256);
+
+      let pre = doc.body.appendChild(doc.createElement("pre"));
+      pre.style.color = textColors[type];
+      pre.style.backgroundColor = "rgba(" + bgColor + ", .2)";
+      pre.appendChild(doc.createTextNode(line));
+
+      // Color another line if there is more
+      if (matches.length > 0)
+        colorText(doc);
+    }, 0);
+
+    // The second frame load is for switching to html
+    let handleHtmlLoad = function SyncLog__handleHtmlLoad(event) {
+      frame.removeEventListener("load", handleHtmlLoad, true);
+      colorText(event.target);
+    };
+    frame.addEventListener("load", handleHtmlLoad, true);
+
+    let html = "<html><head><style>pre{margin:0}</style><body></body></html>";
+    frame.setAttribute("src", "data:text/html," + html);
   },
 
   onSelectionChanged: function SyncLog_onSelectionChanged(event) {
