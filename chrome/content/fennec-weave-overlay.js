ea99a4195a5f34d3db7deecd9091c66537daf678
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2009-03-01 21:26:22 -0800

    Fixed the XBL for Richpref type String, so the Client Name and Server URL preferences on Fennec work now.

--- chrome/content/fennec-weave-overlay.js
+++ chrome/content/fennec-weave-overlay.js
@@ -185,21 +185,14 @@ FennecWeaveGlue.prototype = {
 
   openPrefsPane: function FennecWeaveGlue__openPrefsPane() {
     BrowserUI.switchPane("weave-detail-prefs-pane");
-    var clientName = this._pfs.getCharPref("extensions.weave.client.name");
     var username = this._pfs.getCharPref("extensions.weave.username");
-    var serverUrl = this._pfs.getCharPref("extensions.weave.serverURL");
-
     var theButton = document.getElementById("weave-on-off-button");
     if (this._pfs.getBoolPref("extensions.weave.enabled")) {
       theButton.label = "Turn Weave Off";
     } else {
       theButton.label = "Turn Weave On";
     }
-    //document.getElementById("client-name-input").value = clientName;
-    //document.getElementById("server-url-input").value = serverUrl;
     document.getElementById("username-label").value = "You are user: " + username;
-    // TODO update client name and server URL when user changes value
-    // in fields.  Or make them richpref
   },
 
   openWeavePane: function FennecWeaveGlue__openWeavePane() {
